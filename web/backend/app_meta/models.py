6342e89c7553f3f88d8b54387db2aeea2aed9055
vovet12 <www.vova121998@gmail.com>
2018-04-26 15:31:25 +0300

    AppMeta model builded

--- web/backend/app_meta/models.py
+++ web/backend/app_meta/models.py
@@ -3,3 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
+class AppMeta(db.Model):
+    id_num = db.Column(db.Integer)
+    id_str = db.Column(db.String(128))
+    name = db.Column(db.String(128))
+    hl = db.Column(db.String(2), default='en')
+    content = db.Column(db.String())
+
+    def __init__(self, id_str, name, hl, content):
+        self.id_str = id_str
+        self.name = name
+        self.hl = hl
+        self.content = content
